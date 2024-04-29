f5582eee286e14e0e9dab2aad2bc5ace74845d4c
gucchi <49954787+websuke@users.noreply.github.com>
2024-04-29 19:59:13 +0900

    tailwind cssの反映されるタイミングがワンテンポ遅いため色々試してみる。

--- tailwind.exec.sh
+++ tailwind.exec.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-npx tailwindcss -i styles/input.css -o ./src/main/resources/static/css/main.css
+npx tailwindcss -i styles/input.css -o ./src/main/resources/static/css/main.css --watch
