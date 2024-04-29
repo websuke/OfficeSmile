da1e24ae123aa71a1542d639f4583dc4817ad308
gucchi <49954787+websuke@users.noreply.github.com>
2024-04-29 14:40:42 +0900

    1st commit

---
+++ src/main/java/com/example/officesmile/OfficeSmileApplication.java
@@ -0,0 +1,13 @@
+package com.example.officesmile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeSmileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeSmileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/officesmile/OfficeSmileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.officesmile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeSmileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
