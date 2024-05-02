29bf963434e2bbccd70bda9d6f0bc865a6827cb5
gucchi <49954787+websuke@users.noreply.github.com>
2024-05-03 05:12:31 +0900

    Transactional追記

--- src/main/java/com/example/officesmile/domain/useCase/adminOrAbove/UserStoreUseCase.java
+++ src/main/java/com/example/officesmile/domain/useCase/adminOrAbove/UserStoreUseCase.java
@@ -8,6 +8,7 @@ import com.example.officesmile.mapper.user.UserMapper;
 import com.example.officesmile.mapper.userRole.UserRoleMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +17,7 @@ public class UserStoreUseCase {
     private final UserMapper userMapper;
     private final UserRoleMapper userRoleMapper;
 
+    @Transactional
     public void invoke(String userName, String authId, String password, String authority, String roleId) {
         authMapper.store(AuthEntity.toEntity(authId, password, authority));
 
