ac9c34330b642f10e16d15fce4251e5f8d113e1e
gucchi <49954787+websuke@users.noreply.github.com>
2024-05-02 18:23:29 +0900

    ログインは全社員必須に変更

--- src/main/java/com/example/officesmile/config/SecurityConfig.java
+++ src/main/java/com/example/officesmile/config/SecurityConfig.java
@@ -18,13 +18,12 @@ public class SecurityConfig {
                 .logout((logout) -> logout.permitAll())                               // ログアウト処理のPOST /logout は認証無しでアクセス可能
                 .formLogin((form) -> form                                             // フォーム認証設定を開始する
                         .loginPage("/login")                                          // ログインページ表示のためのハンドラーメソッドは GET /login
+                        .defaultSuccessUrl("/")                                       // ログイン後のハンドラーメソッドは GET /
                         .permitAll()                                                  // 認証無しでアクセス可能
                 )
                 .authorizeHttpRequests((requests) -> requests                         // URL毎の認可設定を開始する
                         .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll() // "/h2-console**" へはログインなしでもアクセス可能 ※開発環境のみとする必要あり
                         .requestMatchers("css/**").permitAll()               // CSSへの認証も不要
-                        .requestMatchers("/").permitAll()                    // "/" へはログインなしでもアクセス可能
-                        .requestMatchers("/returning-to-works").permitAll()  // "/returning-to-works" へはログインなしでもアクセス可能
                         .anyRequest().authenticated()                                 // その他のURLへはログイン後アクセス可能
                 )
                 .csrf(csrf -> csrf.ignoringRequestMatchers(
