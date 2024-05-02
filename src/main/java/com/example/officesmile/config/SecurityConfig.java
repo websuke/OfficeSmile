6d9f5d67a51cfed2764fbdf13006893d4d3af5ef
gucchi <49954787+websuke@users.noreply.github.com>
2024-05-02 21:40:56 +0900

    固定値でログインできるように実装

--- src/main/java/com/example/officesmile/config/SecurityConfig.java
+++ src/main/java/com/example/officesmile/config/SecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -35,6 +39,18 @@ public class SecurityConfig {
 
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("1111")
+                        .password("password")
+                        .roles("GENERAL")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 }
 
