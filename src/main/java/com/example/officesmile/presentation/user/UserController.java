691e53ae60d61f4f7fea3a47da0301b13733ca0b
gucchi <49954787+websuke@users.noreply.github.com>
2024-05-06 02:57:02 +0900

    Validationの仮実装

--- src/main/java/com/example/officesmile/presentation/user/UserController.java
+++ src/main/java/com/example/officesmile/presentation/user/UserController.java
@@ -6,7 +6,10 @@ import com.example.officesmile.domain.useCase.adminOrAbove.UserStoreUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -20,7 +23,7 @@ public class UserController {
     private final AuthPullDownGetUseCase authPullDownGetUseCase;
 
     @GetMapping("/signup")
-    public String showSignUpForm(Model model) {
+    public String showSignUpForm(@ModelAttribute UserForm form, Model model) {
         model.addAttribute("rolePullDown", rolePullDownGetUseCase.invoke());
         model.addAttribute("authPullDown", authPullDownGetUseCase.invoke());
 
@@ -28,7 +31,11 @@ public class UserController {
     }
 
     @PostMapping
-    public String store(UserForm form) {
+    public String store(@Validated @ModelAttribute UserForm form, BindingResult result) {
+        if (result.hasErrors()) {
+
+            return "/user/signup";
+        }
 
         userStoreUseCase.invoke(form.userName(), form.authId(), form.password(), form.authority(), form.roleId());
 
--- src/main/java/com/example/officesmile/presentation/user/UserForm.java
+++ src/main/java/com/example/officesmile/presentation/user/UserForm.java
@@ -1,10 +1,24 @@
 package com.example.officesmile.presentation.user;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public record UserForm (
+        @NotBlank
+        @Size(max = 128)
         String authId,
+        @NotBlank
+        @Size(max = 128)
         String password,
+        @NotBlank
+        @Size(max = 256)
         String userName,
+        @NotBlank
+        @Pattern(regexp = "GENERAL|ADMIN|SUPER_USER", message = "一般, 管理者, スーパーユーザーのいずれかを入力して下さい。")
         String authority,
+        @NotBlank
+//      TODO: @DBの値を見て存在しない場合にエラーとしたいためカスタムバリデーションを作成する。
         String roleId
 ){
 
