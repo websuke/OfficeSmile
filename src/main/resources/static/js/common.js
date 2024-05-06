95b528f7d4e6aeac5c167a7adf55c82625949443
gucchi <49954787+websuke@users.noreply.github.com>
2024-05-06 09:42:23 +0900

    async awaitの記述に修正

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -2,33 +2,41 @@
 
 const delay = 40 // fps=25
 
-const fadeOut = function() {
-    return new Promise(async (resolve, reject) => {
-        if (arguments.length > 0) {
-            const elem = arguments[0]
-            if (elem.style.display !== 'none') {
-                const duration = arguments.length === 2 ? arguments[1] : 500
+const fadeOut = async function(elem, duration = 500) {
+    if (!elem) {
+        throw new Error('elem is null')
+    }
 
-                const coef = 1 / (duration / delay)
-                elem.style.opacity = 1.0
-                let opacity = Number(elem.style.opacity)
-                let animation = setInterval(() => {
-                    opacity -= coef
-                    elem.style.opacity = opacity
-                    if (opacity <= 0) {
-                        elem.style.display = 'none'
-                        clearInterval(animation)
-                        resolve(true)
-                    }
-                }, delay)
-            }
-        }
-    })
+    if (elem.style.display === 'none') {
+        return true // すでに非表示の場合は処理を終了
+    }
+
+    // delayの定義が必要
+    const delay = 10
+
+    // opacityを減少させてフェードアウトさせるアニメーション
+    const coef = 1 / (duration / delay)
+    elem.style.opacity = 1.0
+    let opacity = Number(elem.style.opacity)
+
+    // アニメーションを待機する関数を定義
+    function wait(delay) {
+        return new Promise(resolve => setTimeout(resolve, delay))
+    }
+
+    while (opacity > 0) {
+        await wait(delay)
+        opacity -= coef
+        elem.style.opacity = opacity
+    }
+
+    elem.style.display = 'none'
+    return true
 }
 
-const elem = document.querySelector("#flashMessage")
-if (elem !== null) {
+const flashMessage = document.querySelector("#flashMessage")
+if (flashMessage !== null) {
     setTimeout(() => {
-        fadeOut(elem, 2000)
+        fadeOut(flashMessage, 2000)
     }, 2000)
 }
